346c56bdce664d46bf78e6340a7212e4414e6a0a
Mazen Sheeha <147012970+DMazenSheeha@users.noreply.github.com>
2024-08-27 18:10:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Home from "./pages/Home/Home";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect, useState } from "react";
 
+const noneStyle = {
+  display: "none",
+};
+
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
@@ -12,33 +16,34 @@ function App() {
   }, []);
   return (
     <>
-      {!isLoaded ? (
-        <div
-          style={{
-            height: "100vh",
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <span className="loader"></span>
-        </div>
-      ) : (
-        <div className="app">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="*"
-              element={
-                <h1 style={{ margin: "50px auto", textAlign: "center" }}>
-                  404 <br /> Page Not Found
-                </h1>
+      <div
+        style={
+          isLoaded
+            ? noneStyle
+            : {
+                height: "100vh",
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
               }
-            />
-          </Routes>
-        </div>
-      )}
+        }
+      >
+        <span className="loader"></span>
+      </div>
+      <div className="app" style={isLoaded ? {} : noneStyle}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="*"
+            element={
+              <h1 style={{ margin: "50px auto", textAlign: "center" }}>
+                404 <br /> Page Not Found
+              </h1>
+            }
+          />
+        </Routes>
+      </div>
     </>
   );
 }
