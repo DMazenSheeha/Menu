9c5912cba37642d1c621599d957d4302741d4cb6
Mazen Sheeha <147012970+DMazenSheeha@users.noreply.github.com>
2024-08-27 18:06:39 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 1500);
+  }, []);
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="*"
-          element={
-            <h1 style={{ margin: "50px auto", textAlign: "center" }}>
-              404 <br /> Page Not Found
-            </h1>
-          }
-        />
-      </Routes>
-    </div>
+    <>
+      {!isLoaded ? (
+        <div
+          style={{
+            height: "100vh",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <span className="loader"></span>
+        </div>
+      ) : (
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="*"
+              element={
+                <h1 style={{ margin: "50px auto", textAlign: "center" }}>
+                  404 <br /> Page Not Found
+                </h1>
+              }
+            />
+          </Routes>
+        </div>
+      )}
+    </>
   );
 }
 
